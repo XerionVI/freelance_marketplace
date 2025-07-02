5fc3620b4ef39dd05d2719924f45b551e4cab684
alexandro <alexandro.wibowo86@gmail.com>
2025-07-02 23:05:23 +0700

    config update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_BASE_URL: "http://localhost:5000", // Backend server URL
-  SOCKET_URL: "http://localhost:5000", // Socket.IO server URL
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL, // Backend server URL
+  SOCKET_URL: import.meta.env.VITE_API_BASE_URL, // Socket.IO server URL
   CONTRACT_ADDRESS: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
   DISPUTE_RESOLUTION_ADDRESS: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // Replace with actual dispute resolution contract address
   VOTING_MODULE_ADDRESS: "0x5FbDB2315678afecb367f032d93F642f64180aa3", // Replace with actual voting module contract address
