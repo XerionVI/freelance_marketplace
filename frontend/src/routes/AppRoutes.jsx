b8ebbedba45f454dbe7775ea5cae66e29d206c6f
alexandro <alexandro.wibowo86@gmail.com>
2025-08-04 20:46:19 +0700

    backend changes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import AuthForm from "../components/auth/AuthForm";
 import JobDetailsPage from "../components/jobs/JobDetails/JobDetailsPage";
 import UserProfile from "../components/User/UserProfile";
 import FreelancerHome from "../components/marketPage/freelancerPage/FreelancerHome";
-import TransactionHome from "../components/transcation/TransactionHome";
 import JobManagementHome from "../components/jobs/JobManagement/JobManagementHome";
 import DisputeHome from "../components/disputes/disputePage/DisputeHome";
 import ListingsHome from "../components/marketPage/jobPage/ListingsHome";
@@ -24,7 +23,6 @@ function AppRoutes({ account, token, handleAuthSuccess, handleLogin, handleRegis
       <Route path="/freelancer-home" element={<FreelancerHome account={account} token={token} />} />
       <Route path="/users/:id/profile" element={<UserProfile account={account} />} />
       <Route path="/history" element={<HistoryHome account={account} token={token} />} />
-      {/* <Route path="/transaction" element={<TransactionHome account={account}/>} /> */}
       <Route path="/disputes" element={<DisputeHome account={account} token={token} />} />
       <Route path="/job-management" element={<JobManagementHome account={account} token={token} />} />
       <Route path="/listings" element={<ListingsHome account={account} token={token} />} />
